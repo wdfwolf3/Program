dcb12dab1ebd9ae16bd9c2af150ecb8752cce9c5
wdfwolf3 <fwolff@vip.qq.com>
2017-07-03 23:32:32 +0800

    update

--- src/program/win/MouseLLHook.java
+++ src/program/win/MouseLLHook.java
@@ -38,7 +38,29 @@ public class MouseLLHook {
                 int w = wParam.intValue();
                 // 按下alt键时w=WinUser.WM_SYSKEYDOWN; 按下其他大部分键时w=WinUser.WM_KEYDOWN
                 if (w == WinUser.WM_KEYDOWN || w == WinUser.WM_SYSKEYDOWN){
-                    System.out.println("key down: vkCode = " + displayCode(lParam.vkCode));
+                    if(displayCode(lParam.vkCode)=='C'){
+                        WinDef.HWND h1 = User32.INSTANCE.GetForegroundWindow();
+//        User32.INSTANCE.MoveWindow(h1,10,10,10,10,true);
+                        WinUser.INPUT input = new WinUser.INPUT(  );
+                        Character ch = 'a';
+                        input.type = new WinDef.DWORD( WinUser.INPUT.INPUT_KEYBOARD );
+                        input.input.setType("ki"); // Because setting INPUT_INPUT_KEYBOARD is not enough: https://groups.google.com/d/msg/jna-users/NDBGwC1VZbU/cjYCQ1CjBwAJ
+                        input.input.ki.wScan = new WinDef.WORD( 0 );
+                        input.input.ki.time = new WinDef.DWORD( 0 );
+                        input.input.ki.dwExtraInfo = new BaseTSD.ULONG_PTR( 0 );
+                        // Press
+                        input.input.ki.wVk = new WinDef.WORD( Character.toUpperCase(ch) ); // 0x41
+                        input.input.ki.dwFlags = new WinDef.DWORD( 0 );  // keydown
+
+                        User32.INSTANCE.SendInput( new WinDef.DWORD( 1 ), ( WinUser.INPUT[] ) input.toArray( 1 ), input.size() );
+
+                        // Release
+                        input.input.ki.wVk = new WinDef.WORD( Character.toUpperCase(ch) ); // 0x41
+                        input.input.ki.dwFlags = new WinDef.DWORD( 2 );  // keyup
+
+                        User32.INSTANCE.SendInput( new WinDef.DWORD( 1 ), ( WinUser.INPUT[] ) input.toArray( 1 ), input.size() );
+                    }
+//                    System.out.println("key down: vkCode = " + displayCode(lParam.vkCode));
 //                    print();
                 }
 
